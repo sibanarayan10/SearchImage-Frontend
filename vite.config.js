51cebba9335569ddb99c3645f32d3d355179656c
sibanarayan10 <sibanarayan014@gmail.com>
2026-01-28 22:52:10 +0530

    Configuration:production related

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
