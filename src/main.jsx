ef5f25bbbebb8042ed0487985636608e015b11d2
sibanarayan10 <sibanarayan014@gmail.com>
2026-02-01 10:09:15 +0530

    Manages file and folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,5 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ContextProvider } from "./Context/globalContext";
 
-createRoot(document.getElementById("root")).render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>
-);
+createRoot(document.getElementById("root")).render(<App />);
